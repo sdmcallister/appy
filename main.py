8b9af7c2dafcb0b10c55ba6c34689c3384a308d9
steve <sdmcallister@gmail.com>
2022-04-02 16:37:42 -0400

    feat: basic auth

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+import secrets
+import os
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+security = HTTPBasic()
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(
+        credentials.username, os.getenv("USERNAME"))
+    correct_password = secrets.compare_digest(
+        credentials.password, os.getenv("PASSWORD"))
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(username: str = Depends(get_current_username)):
+    return {"message": f'Hello, {username}'}
